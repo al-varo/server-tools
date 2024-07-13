fbe6e0eb0473d3ce2955ae6ac7e0f1383187af70
al-varo <52884208+al-varo@users.noreply.github.com>
2024-07-13 13:30:53 +0700

    Create __openerp__.py

---
+++ manzada_log/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+########################################################################
+#
+# @authors: Fauzi Rahman, M.Kom <fauzi@manzada.net>
+# Copyright (C) 2018  Fauzi Rahman, M.Kom
+#
+########################################################################
+
+{
+    'name' : 'Manzada Log',
+    'version' : '1.0',
+    'author' : 'Fauzi Rahman, M.Kom',
+    'category' : 'Account',
+    'description' : """
+Mencatat aktifitas user kemudian mengirimkan datanya lewat telegram.
+    """,
+    'website': 'http://dev.manzada.net',
+    'images' : [],
+    'depends' : ['account'],
+    'data': [
+    ],
+    'js': [
+
+    ],
+    'qweb' : [
+
+    ],
+    'css':[
+
+    ],
+    'demo': [
+
+    ],
+    'test': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+}
