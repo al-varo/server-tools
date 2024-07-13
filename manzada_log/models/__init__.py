643aa275a0392a7b110c42808822ce29165cb8bf
al-varo <52884208+al-varo@users.noreply.github.com>
2024-07-13 13:27:33 +0700

    Create __init__.py

---
+++ manzada_log/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+########################################################################
+#
+# @authors: Fauzi Rahman, M.Kom <fauzi@manzada.net>
+# Copyright (C) 2018  Fauzi Rahman, M.Kom
+#
+########################################################################
+import account_invoice
